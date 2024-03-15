19573265ed4bc64b2160a969c48214540e1e4178
Ahegao_Burger <artur.waysman.2004@mail.ru>
2024-03-15 20:47:49 +0000

    Added some UX stuff and working on creating communities.

---
+++ src/hack_frontend/src/routes/setup/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { Button } from '$lib/components/ui/button'
+    import { Input } from '$lib/components/ui/input';
+
+    let smartContractAdress;
+    let communityName;
+
+</script>
+
+<h1>Create a space</h1>
+
+<p>Hack is a free, ICP-based platform for community governance. Create your own space now and start making decisions!</p>
+
+<Button>Get Started</Button>
+
+<Input 
+
+/>
+
+<Input 
+
+/>
+
